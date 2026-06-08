66d9b8f109ab4d92d1ff94dcbe77f714b6c0cf47
Fredianto <fredinix@proton.me>
2026-06-08 17:31:07 +0700

    fix: imports

--- lib/core/widgets/ticker_tape.dart
+++ lib/core/widgets/ticker_tape.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-import '../theme/app_theme.dart';
 import '../../presentation/theme/app_spacing.dart';
 import '../../presentation/theme/app_typography.dart';
+import '../theme/app_theme.dart';
 
 class TickerTape extends StatelessWidget {
   const TickerTape({super.key});
